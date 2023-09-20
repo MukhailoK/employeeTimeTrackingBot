649c989ff5d015e253f98d54f2fc9bb2f1500e23
Mykhailo Kolotailo <kolotajlo17@gmail.com>
2023-09-20 22:30:08 +0200

    Initial commit

---
+++ src/main/java/com/bot/employeeTimeTracongBot/EmployeeTimeTracongBotApplication.java
@@ -0,0 +1,13 @@
+package com.bot.employeeTimeTracongBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTimeTracongBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeTimeTracongBotApplication.class, args);
+	}
+
+}
