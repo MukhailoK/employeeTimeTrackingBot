930a2fc8dd6d18915b18a5ba8a66556190cc7a4e
Mykhailo Kolotailo <kolotajlo17@gmail.com>
2023-09-21 13:31:54 +0200

    initial telegram bot with simple features

---
+++ src/main/java/keys/Key.java
@@ -0,0 +1,6 @@
+package keys;
+
+public class Key {
+    public static final String TELEGRAM_TOKEN = "2119117221:AAEX6vT5nhLvHnAKrQw73OS1wL3BrqdGhyk";
+    public static final String TELEGRAM_BOT_NAME = "@Funy_honey_bot";
+}
